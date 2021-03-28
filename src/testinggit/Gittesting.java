0f8eab5784fff00e7ac6e2465e30c01c63401866
Sandyakarra <karrasandya84@gmail.com>
2021-03-28 13:25:02 +0100

    file modified

--- src/testinggit/Gittesting.java
+++ src/testinggit/Gittesting.java
@@ -5,7 +5,7 @@ public class Gittesting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		// this is modifid and test the message
 		
 	// i am trying to test the git
 		
