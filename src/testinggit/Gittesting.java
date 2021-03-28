e0432b403ad5a964f70a2daab512b79339e2fac4
Sandyakarra <karrasandya84@gmail.com>
2021-03-28 13:19:27 +0100

    changes made today

---
+++ src/testinggit/Gittesting.java
@@ -0,0 +1,15 @@
+package testinggit;
+
+public class Gittesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	// i am trying to test the git
+		
+
+	}
+
+}
